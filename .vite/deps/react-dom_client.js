43117a6c47dab56d9ddd96f89d0dd111fb8ca7c3
Carlos R Andujar <iamcrandujar@gmail.com>
2024-01-17 14:26:27 -0500

    Revert "Merge branch 'carlos' of https://github.com/horchataclub/citypopthreejs into carlos"
    This reverts commit 00f84b2ffa1815c86b62799305d6e9bd8129ac26, reversing
    changes made to a746a89c9b2c847431c93a15da85ff9689a4dd45.

--- .vite/deps/react-dom_client.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "./chunk-HQZ5IH36.js";
-import "./chunk-2MRMY2VI.js";
-import "./chunk-6DDWND5A.js";
-import "./chunk-4EOJPDL2.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Effects from './Effects'
 import Torus from './Torus'
 import { useControls } from 'leva'
 import { useThree } from '@react-three/fiber'
-import { CameraHelper, LinearSRGBColorSpace, Mesh, SRGBColorSpace, MeshPhysicalMaterial, MeshStandardMaterial, TorusKnotGeometry, DirectionalLightHelper } from 'three'
+import { CubeTextureLoader, CameraHelper, LinearSRGBColorSpace, Mesh, SRGBColorSpace, MeshPhysicalMaterial, MeshStandardMaterial, TorusKnotGeometry, DirectionalLightHelper, TetrahedronGeometry } from 'three'
 import { useEffect, useRef, useState } from 'react'
 import RoundedCube from './RoundedCube'
 import Hills from './Hills'
@@ -16,7 +16,11 @@ import Wind from './Wind'
 import Ribbons from './Ribbons'
 import { SignMerge } from './SignMerge'
 import { Billboard } from './Billboard'
-import { Ground2 } from './Ground2'
+import { Street } from './Street'
+import * as THREE from 'three' 
+import { Barbershop } from './Barbershop'
+
+
 
 
 function App() {
@@ -28,11 +32,12 @@ function App() {
       gl.gammaInput = true;
       gl.gammaOutput = true;
       //gl.stencil = true;
+      //gl.shadowMap.type = THREE.VSMShadowMap;
     }
   }, [gl]);
 
   const { intensity } = useControls({
-    intensity: 1.7
+    intensity: 1.7 
   });
  
   const { SunDir }= useControls({
@@ -42,33 +47,64 @@ function App() {
     ShadowBias: { value: 0.0, label: 'Shadow Bias', step: .001, min: -2, max: 2 }
   })
  
+  const mainCam = useRef()
   const dirLight = useRef()
   const shadowCam = useRef()
+
+  //useHelper(mainCam, CameraHelper )
   //useHelper(dirLight, DirectionalLightHelper, 1, "red");
   //useHelper(shadowCam, CameraHelper)
+
 
   // const { groundPosition } = useControls({
   //   groundPosition: { value: [0.0, 0.0, 0.0], label: 'Ground Position', step: 0.5, min: -20, max: 20 }
   // })
+
+  
+
+
   
   return (
     <>
        {/* <axesHelper args={[5, 5, 5]} /> */}
-      <color args={[ '#364d81' ]} attach="background" />      
-      <OrbitControls  target={[1, 3, 0]} />
-      <PerspectiveCamera makeDefault rotation={[10,50,0]} fov={45} near={0.1} far={200} position={[ 2, 6, 6 ]}>
+      <color args={[ '#364d81' ]} attach="background" /> 
+
+      <OrbitControls target={[1, 3, 0]}
+
+        // constraints---------------------
+
+        // enablePan={false}
+        // enableZoom={false}
+        // minAzimuthAngle={-Math.PI / 4}
+        // maxAzimuthAngle={Math.PI / 4}
+        // minPolarAngle={Math.PI / 6}
+        // maxPolarAngle={Math.PI / 2} 
+
+      />
+
+      <PerspectiveCamera 
+        makeDefault 
+        ref={mainCam} 
+        rotation={[10,50,0]} 
+        fov={45} 
+        near={0.1} 
+        far={200} 
+        position={[.77, 0.8, 10]}
+      >
         <Dots />
         <Wind />
         <Ribbons />
       </PerspectiveCamera>
       <Effects />
       
-      <ambientLight intensity={0.00} />
+      <ambientLight intensity={.08} color={"#234a99"} />
       <directionalLight 
         color="white" 
         //shadow-bias={-0.003} 
         shadow-bias={ 0 }
+        //shadow-radius={ 0 }
         shadow-mapSize={[2048, 2048]}
+       // shadow-mapSize={[1024, 1024]}
         ref={ dirLight } 
         position={SunDir} 
         intensity={ intensity } 
@@ -84,9 +120,10 @@ function App() {
       <Car position={ [0, 0, 3]} />
       <SignMerge />
       <Hills />
-      <Bush />      
-      <Ground2 rotation-y={1.6} scale={3.5} position={[-2, 0, -1]} />
-      <Billboard />      
+      <Bush />   
+      <Street rotation-y={1.6} scale={3.7} position={[-2, 0, -1]} />
+      <Billboard /> 
+      <Barbershop scale={ 1.4 } rotation-y={ 1.6}  position={[-8, 0, 1]} />
     </>
   )
 } 
